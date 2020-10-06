d13bbf8076f0cdf07a9a2e792cee24292491278b
thomasvega <dontgonzalezvega@gmail.com>
2020-10-06 11:55:28 +0200

    Adding user creation to urlspattern

--- pollsapi/pollsapi/urls.py
+++ pollsapi/pollsapi/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from views import PollViewSet ChoiceList, CreateVote
+from views import PollViewSet ChoiceList, CreateVote, UserCreate
 
 router = DefaultRouter()
 router.register('polls', PollViewSet, basename="polls")
@@ -25,6 +25,7 @@ router.register('polls', PollViewSet, basename="polls")
 urlpatterns = [
     path('polls/<int:pk>/choices/', ChoiceList.as_view(), name="choice_list"),
     path('polls/<int:pk>/choices/<int:choice_pk>/vote/', CreateVote.as_view(), name="create_vote"),
+    path('users/', UserCreate.as_view(), name="user_create"),
 ]
 
 urlpatterns += router.urls

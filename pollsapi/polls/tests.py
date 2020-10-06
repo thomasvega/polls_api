12e35225a7a8fa622cedf9b45710e84557d52b27
thomasvega <dontgonzalezvega@gmail.com>
2020-10-06 13:03:04 +0200

    Using vanilla testing

--- pollsapi/polls/tests.py
+++ pollsapi/polls/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework.test import APIRequestFactory
+from rest_framework.authtoken.models import Token
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+from polls import views
+
+class TestPoll(APITestCase):
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.view = views.PollViewSet.as_view({'get': 'list'})
+        self.uri = '/polls/'
+        self.user = self.setup_user()
+        self.token = Token.objects.create(user=self.user)
+        self.token.save()
+    
+    @staticmethod
+    def setup_user():
+        User = get_user_model()
+        return User.objects.create_user(
+            'test',
+            email='testuser@test.com',
+            password='test'
+        )
+
+    def test_list(self):
+        request = self.factory.get(self.uri,
+            HTTP_AUTHORIZATION='Token {}'.format(self.token.key))
+        request.user = self.user
+        response = self.view(request)
+        self.assertEqual(response.status_code, 200, 'Excepted Response Code 200, received {0} instead.'.format(response.status_code))

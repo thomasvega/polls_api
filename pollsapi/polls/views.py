5dce3904284f9f8d533c615a28eb1053709ae908
thomasvega <dontgonzalezvega@gmail.com>
2020-10-06 12:51:16 +0200

    Adding security. Making sure that the delete method is only possible if you created a poll and add choice method only possible if you are the owner of the poll

--- pollsapi/polls/views.py
+++ pollsapi/polls/views.py
@@ -3,6 +3,8 @@ from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework import viewsets
+from django.contrib.auth import authenticate
+from rest_framework.exceptions import PermissionDenied
 
 from .serializers import PollSerializer, ChoiceSerializer, VoteSerializer, UserSerializer
 from .models import Poll, Choice, Vote
@@ -12,11 +14,32 @@ class PollViewSet(viewsets.ModelViewSet):
     queryset = Poll.objects.all()
     serializer_class = PollSerializer
 
+    def destroy(self, request, *args, **kwargs):
+        """
+        Overriding destroy method to make sure that it's only
+        the user who created the poll that is able to delete it
+        """
+        poll = Poll.objects.get(pk=self.kwargs['pk'])
+        if not request.user == poll.created_by:
+            raise PermissionDenied("You can not delete this poll.")
+        return super().destroy(request, *args, **kwargs)
+
 class ChoiceList(generics.ListCreateAPIView):
     def get_queryset(self):
         queryset = Choice.objects.filter(poll_id=self.kwargs['pk'])
         return queryset
     serializer_class = ChoiceSerializer
+
+    def post(self, request, *args, **kwargs):
+        """
+        Overriding post method to make sure that it's only
+        the user who created the poll that is able to add choice
+        """
+        poll = Poll.objects.get(pk=self.kwargs['pk'])
+        if not request.user == poll.created_by:
+            raise PermissionDenied("You can not create choice for this poll.")
+        return self().post(request, *args, **kwargs)
+
 
 class CreateVote(APIView):
     serializer_class = VoteSerializer
@@ -39,3 +62,15 @@ class UserCreate(generics.CreateAPIView):
     authentication_classes = ()
     permission_classes = ()
     serializer_class = UserSerializer
+
+class LoginView(APIView):
+    permission_classes = ()
+
+    def post(self, request,):
+        username = request.data.get('username')
+        password = request.data.get('password')
+        user = authenticate(username=username, password=password)
+        if user:
+            return Response({"token": user.auth_token.key})
+        else:
+            return Response({"error": "Wrong Credentials"}, status=status.HTTP_400_BAD_REQUEST)
--- pollsapi/pollsapi/urls.py
+++ pollsapi/pollsapi/urls.py
@@ -17,12 +17,13 @@ from django.contrib import admin
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
-from polls.views import PollViewSet, ChoiceList, CreateVote, UserCreate
+from polls.views import PollViewSet, ChoiceList, CreateVote, UserCreate, LoginView
 
 router = DefaultRouter()
 router.register('polls', PollViewSet, basename="polls")
 
 urlpatterns = [
+    path('login/', LoginView.as_view(), name="login"),
     path('polls/<int:pk>/choices/', ChoiceList.as_view(), name="choice_list"),
     path('polls/<int:pk>/choices/<int:choice_pk>/vote/', CreateVote.as_view(), name="create_vote"),
     path('users/', UserCreate.as_view(), name="user_create"),

aadf97901900bfa5249f6220d3ee29f9d880b00e
thomasvega <dontgonzalezvega@gmail.com>
2020-10-06 13:09:27 +0200

    Testing post

--- pollsapi/polls/tests.py
+++ pollsapi/polls/tests.py
@@ -36,3 +36,13 @@ class TestPoll(APITestCase):
         self.client.login(username="test", password="test")
         response = self.client.get(self.uri)
         self.assertEqual(response.status_code, 200, 'Excepted Response Code 200, received {0} instead.'.format(response.status_code))
+    
+    def test_create(self):
+        self.client.login(username="test", password="test")
+        params = {
+            "question": "How are you?",
+            "created_by": 1
+        }
+        response = self.client.post(self.uri, params)
+        self.assertEqual(response.status_code, 201,
+                        'Expected Response Code 201, received {0} instead.'.format(response.status_code))

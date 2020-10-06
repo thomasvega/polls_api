547bd7dbb826635b0109daee7c35eab3374dd284
thomasvega <dontgonzalezvega@gmail.com>
2020-10-06 12:06:46 +0200

    Making sure the token will be created when a user is created aswell

--- pollsapi/polls/serializers.py
+++ pollsapi/polls/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
 
 from .models import Poll, Choice, Vote
 
@@ -41,4 +42,5 @@ class UserSerializer(serializers.ModelSerializer):
         )
         user.set_password(validated_data['password'])
         user.save()
+        Token.objects.create(user=user)
         return user
